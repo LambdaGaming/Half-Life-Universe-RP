29b32a2180842dc55e3ac458adcbdc19c56d1555
LambdaGaming <starwars18m@gmail.com>
2019-04-16 10:01:00 -0400

    Added comment

--- lua/autorun/soundfix.lua
+++ lua/autorun/soundfix.lua
@@ -1,5 +1,6 @@
 
 --Fixes some sounds not being able to be heard on rp_sectorc_beta like the tram announcements and door sounds
+--Also reduces the sound of the alarms on rp_bmrf so players don't go deaf the moment the round starts
 hook.Add( "InitPostEntity", "join_con_commands", function()
     if game.GetMap() == "rp_sectorc_beta" then
 		RunConsoleCommand( "snd_restart" )
