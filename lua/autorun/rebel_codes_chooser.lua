023e957818135503128bb2b311cd0f53649a06bf
LambdaGaming <starwars18m@gmail.com>
2019-12-05 15:38:35 -0500

    Fixed script running on gamemodes other than Outland RP

--- lua/autorun/rebel_codes_chooser.lua
+++ lua/autorun/rebel_codes_chooser.lua
@@ -1,4 +1,11 @@
 
+local allowedmaps = {
+	["gm_boreas"] = true,
+	["rp_ineu_valley2_v1a"] = true
+}
+
+if !allowedmaps[game.GetMap()] then return end
+
 --Run command if things go haywire:
 --lua_run for k,v in pairs( ents.FindByClass( [ENTCLASS] ) ) do v.hascodes = true end
 
@@ -15,14 +22,12 @@ local outlandents = {
 }
 
 hook.Add( "InitPostEntity", "OutlandPostEnt", function()
-	if GAMEMODE_NAME != "outlandrp" then return end
 	for k,v in pairs( ents.FindByClass( table.Random( outlandents ) ) ) do
 		v.hascodes = true
 	end
 end )
 
 hook.Add( "PlayerDeath", "OutlandPlayerDeath", function(victim, inflictor, attacker)
-	if GAMEMODE_NAME != "outlandrp" then return end
 	if victim.hascodes == true then
 		victim.hascodes = nil
 		for k,v in pairs( ents.FindByClass( table.Random( outlandents ) ) ) do
@@ -35,7 +40,6 @@ hook.Add( "PlayerDeath", "OutlandPlayerDeath", function(victim, inflictor, attac
 end )
 
 hook.Add( "OnPlayerChangedTeam", "OutlandPlayerChange", function(ply, before, after)
-	if GAMEMODE_NAME != "outlandrp" then return end
 	if ply.hascodes == true and combinejobs[after] then
 		ply.hascodes = nil
 		for k,v in pairs( ents.FindByClass( table.Random( outlandents ) ) ) do
