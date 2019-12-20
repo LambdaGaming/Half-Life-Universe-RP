fa6e6c85af119aad0a521eb0fbb6b6f51f6cf0c0
LambdaGaming <starwars18m@gmail.com>
2019-12-20 17:42:28 -0500

    Added rebel invasion event

--- lua/autorun/c17_events.lua
+++ lua/autorun/c17_events.lua
@@ -1,67 +1,70 @@
 
-if GAMEMODE_NAME == "city17rp" then
+local c17maps = {
+	["rp_city17_build210"] = true,
+	["rp_city17_district47"] = true,
+	["rp_city24_v2"] = true
+}
+if SERVER and c17maps[game.GetMap()] then
 	local zombiepos = {}
 	local indexnum = {}
 	local racexpos = {}
 	local canpos = {}
 
 	function ZombieInvasion()
-		for k,v in pairs(player.GetAll()) do
+		for k,v in pairs( player.GetAll() ) do
 			v:ChatPrint("Zombies From Ravenholm have ended up in the sewers! Please stay indoors until civil protection units deal with the situation.")
 		end
-		if SERVER then
-			if game.GetMap() == "rp_city17_build210" then
-				zombiepos = {
-					Vector( 384, 5641, -466 ),
-					Vector( 601, 5753, -466 ),
-					Vector( 185, 5527, -498 ),
-					Vector( 424, 5009, -464 ),
-					Vector( 1741, 4837, -466 ),
-					Vector( 1907, 1225, -498 ),
-					Vector( 1901, 3283, -498 ),
-					Vector( 1861, -881, -466 ),
-					Vector( 2343, -716, -466 ),
-					Vector( -926, 6732, -466 )
-				}
-			end
-			if game.GetMap() == "rp_city17_district47" then
-				zombiepos = {
-					Vector( -288, -1117, -64 ),
-					Vector( -49, -1258, -64 ),
-					Vector( -1022, -1284, -64 ),
-					Vector( 724, -1401, -120 ),
-					Vector( 664, -1693, -256 ),
-					Vector( 1718, -2756, -288 ),
-					Vector( 556, -1211, -128 ),
-					Vector( 909, -562, -128 ),
-					Vector( 1201, -292, 128 ),
-					Vector( 1200, -946, 64 )
-				}
-			end
-			if game.GetMap() == "rp_city24_v2" then
-				zombiepos = {
-					Vector( 10976, 7871, 136 ),
-					Vector( 10926, 6589, -56 ),
-					Vector( 10517, 8646, 8 ),
-					Vector( 9024, 8978, 8 ),
-					Vector( 6875, 5983, -512 ),
-					Vector( 9381, 6466, -384 ),
-					Vector( 8964, 7983, -384 ),
-					Vector( 8760, 8164, -991 ),
-					Vector( 9640, 7394, -376 ),
-					Vector( 10328, 8471, 9 )
-				}
-			end
-			local zombies = {
-				"npc_zombie",
-				"npc_poisonzombie",
-				"npc_fastzombie"
+		if game.GetMap() == "rp_city17_build210" then
+			zombiepos = {
+				Vector( 384, 5641, -466 ),
+				Vector( 601, 5753, -466 ),
+				Vector( 185, 5527, -498 ),
+				Vector( 424, 5009, -464 ),
+				Vector( 1741, 4837, -466 ),
+				Vector( 1907, 1225, -498 ),
+				Vector( 1901, 3283, -498 ),
+				Vector( 1861, -881, -466 ),
+				Vector( 2343, -716, -466 ),
+				Vector( -926, 6732, -466 )
 			}
-			for k,v in ipairs( zombiepos ) do
-				local zombie = ents.Create( tostring( table.Random( zombies ) ) )
-				zombie:SetPos( v )
-				zombie:Spawn()
-			end
+		end
+		if game.GetMap() == "rp_city17_district47" then
+			zombiepos = {
+				Vector( -288, -1117, -64 ),
+				Vector( -49, -1258, -64 ),
+				Vector( -1022, -1284, -64 ),
+				Vector( 724, -1401, -120 ),
+				Vector( 664, -1693, -256 ),
+				Vector( 1718, -2756, -288 ),
+				Vector( 556, -1211, -128 ),
+				Vector( 909, -562, -128 ),
+				Vector( 1201, -292, 128 ),
+				Vector( 1200, -946, 64 )
+			}
+		end
+		if game.GetMap() == "rp_city24_v2" then
+			zombiepos = {
+				Vector( 10976, 7871, 136 ),
+				Vector( 10926, 6589, -56 ),
+				Vector( 10517, 8646, 8 ),
+				Vector( 9024, 8978, 8 ),
+				Vector( 6875, 5983, -512 ),
+				Vector( 9381, 6466, -384 ),
+				Vector( 8964, 7983, -384 ),
+				Vector( 8760, 8164, -991 ),
+				Vector( 9640, 7394, -376 ),
+				Vector( 10328, 8471, 9 )
+			}
+		end
+		local zombies = {
+			"npc_zombie",
+			"npc_poisonzombie",
+			"npc_fastzombie"
+		}
+		for k,v in ipairs( zombiepos ) do
+			local zombie = ents.Create( tostring( table.Random( zombies ) ) )
+			zombie:SetPos( v )
+			zombie:Spawn()
 		end
 	end
 
@@ -122,45 +125,115 @@ if GAMEMODE_NAME == "city17rp" then
 
 	function RaceX() --why did I agree to do this?
 		for k,v in pairs( player.GetAll() ) do
-			v:ChatPrint("Someone screwed up the lore and race X has invaded the citadel!")
+			v:ChatPrint( "Someone screwed up the lore and Race X has invaded the citadel!" )
 		end
-		if SERVER then
-			if game.GetMap() == "rp_city17_build210" then
-				racexpos = {
-					Vector( 3017, -255, 76 ),
-					Vector( 4450, -71, 76 ),
-					Vector( 5236, -1062, 4156 ),
-					Vector( 6237, -1404, 4156 ),
-					Vector( 5676, -2531, 4924 )
-				}
-			end
-			if game.GetMap() == "rp_city17_district47" then
-				racexpos = {
-					Vector( -968, -2523, 384 ),
-					Vector( -800, -1942, 516 ),
-					Vector( -748, -2546, 776 ),
-					Vector( -1039, -2606, 1152 ),
-					Vector( -165, -1807, 1284 )
-				}
-			end
-			if game.GetMap() == "rp_city24_v2" then
-				racexpos = {
-					Vector( 488, 9047, 520 ),
-					Vector( -1324, 8996, 601 ),
-					Vector( -887, 8319, 857 ),
-					Vector( -663, 6911, 867 ),
-					Vector( -1342, 8835, -32 )
-				}
-			end
-			local racexnpcs = {
-				"npc_pitdrone",
-				"monster_shocktrooper",
-				"monster_alien_babyvoltigore"
+		if game.GetMap() == "rp_city17_build210" then
+			racexpos = {
+				Vector( 3017, -255, 76 ),
+				Vector( 4450, -71, 76 ),
+				Vector( 5236, -1062, 4156 ),
+				Vector( 6237, -1404, 4156 ),
+				Vector( 5676, -2531, 4924 )
 			}
-			for k,v in ipairs( racexpos ) do
-				local zombie = ents.Create( tostring( table.Random( racexnpcs ) ) )
-				zombie:SetPos( v )
-				zombie:Spawn()
+		end
+		if game.GetMap() == "rp_city17_district47" then
+			racexpos = {
+				Vector( -968, -2523, 384 ),
+				Vector( -800, -1942, 516 ),
+				Vector( -748, -2546, 776 ),
+				Vector( -1039, -2606, 1152 ),
+				Vector( -165, -1807, 1284 )
+			}
+		end
+		if game.GetMap() == "rp_city24_v2" then
+			racexpos = {
+				Vector( 488, 9047, 520 ),
+				Vector( -1324, 8996, 601 ),
+				Vector( -887, 8319, 857 ),
+				Vector( -663, 6911, 867 ),
+				Vector( -1342, 8835, -32 )
+			}
+		end
+		local racexnpcs = {
+			"npc_pitdrone",
+			"monster_shocktrooper",
+			"monster_alien_babyvoltigore"
+		}
+		for k,v in ipairs( racexpos ) do
+			local zombie = ents.Create( tostring( table.Random( racexnpcs ) ) )
+			zombie:SetPos( v )
+			zombie:Spawn()
+		end
+	end
+
+	hook.Add( "PlayerSpawn", "ActiveShooterRelationship", function( ply )
+		for k,v in pairs( ents.FindByClass( "npc_citizen" ) ) do
+			if ply:isCP() then
+				v:AddEntityRelationship( ply, D_HT, 99 )
+			end
+		end
+	end )
+
+	function RebelInvasion()
+		for k,v in pairs( player.GetAll() ) do
+			v:ChatPrint( "Rebels have invaded the citadel!" )
+		end
+		if game.GetMap() == "rp_city17_build210" then
+			rebelpos = {
+				Vector( 3017, -255, 76 ),
+				Vector( 4450, -71, 76 ),
+				Vector( 5236, -1062, 4156 ),
+				Vector( 6237, -1404, 4156 ),
+				Vector( 5676, -2531, 4924 )
+			}
+		end
+		if game.GetMap() == "rp_city17_district47" then
+			rebelpos = {
+				Vector( -968, -2523, 384 ),
+				Vector( -800, -1942, 516 ),
+				Vector( -748, -2546, 776 ),
+				Vector( -1039, -2606, 1152 ),
+				Vector( -165, -1807, 1284 )
+			}
+		end
+		if game.GetMap() == "rp_city24_v2" then
+			rebelpos = {
+				Vector( 488, 9047, 520 ),
+				Vector( -1324, 8996, 601 ),
+				Vector( -887, 8319, 857 ),
+				Vector( -663, 6911, 867 ),
+				Vector( -1342, 8835, -32 )
+			}
+		end
+		local randmodel = {
+			"models/humans/group03/female_01.mdl",
+			"models/humans/group03/female_02.mdl",
+			"models/humans/group03/female_03.mdl",
+			"models/humans/group03/female_04.mdl",
+			"models/humans/group03/female_06.mdl",
+			"models/humans/group03/female_07.mdl",
+			"models/humans/group03/male_01.mdl",
+			"models/humans/group03/male_02.mdl",
+			"models/humans/group03/male_03.mdl",
+			"models/humans/group03/male_04.mdl",
+			"models/humans/group03/male_05.mdl",
+			"models/humans/group03/male_06.mdl",
+			"models/humans/group03/male_07.mdl",
+			"models/humans/group03/male_08.mdl",
+			"models/humans/group03/male_09.mdl"
+		}
+		for k,v in ipairs( rebelpos ) do
+			local rebel = ents.Create( "npc_citizen" )
+			rebel:SetPos( v )
+			rebel:Spawn()
+			rebel:Activate()
+			rebel:SetModel( table.Random( randmodel ) )
+			rebel:Give( "weapon_smg1" )
+			rebel:SetHealth( 300 )
+			for k,v in pairs( player.GetAll() ) do
+				if v:isCP() then
+					rebel:AddEntityRelationship( v, D_HT, 99 )
+				end
 			end
 		end
 	end
