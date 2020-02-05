fa23343c7873ceec0ac66928b921bcacf26d88b4
LambdaGaming <starwars18m@gmail.com>
2020-02-04 20:49:16 -0500

    Optimization

--- addons/Combine Portal Closing Entity/lua/entities/cancel_button_combine/init.lua
+++ addons/Combine Portal Closing Entity/lua/entities/cancel_button_combine/init.lua
@@ -3,30 +3,31 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Initialize()
-	self:SetModel("models/props_citizen_tech/firetrap_buttonpad.mdl")
-	self:PhysicsInit(SOLID_VPHYSICS)
-	self:SetMoveType(MOVETYPE_VPHYSICS)
-	self:SetSolid(SOLID_VPHYSICS)
-	self:SetUseType(SIMPLE_USE)
+	self:SetModel( "models/props_citizen_tech/firetrap_buttonpad.mdl" )
+	self:PhysicsInit( SOLID_VPHYSICS )
+	self:SetMoveType( MOVETYPE_VPHYSICS )
+	self:SetSolid( SOLID_VPHYSICS )
+	self:SetUseType( SIMPLE_USE )
+
 	local phys = self:GetPhysicsObject()
-	phys:Wake()
+	if IsValid( phys ) then
+		phys:Wake()
+	end
 	
-	local redlight = ents.Create("light_dynamic")
+	local redlight = ents.Create( "light_dynamic" )
 	redlight:SetPos( self:GetPos() )
 	redlight:SetOwner( self:GetOwner() )
-	redlight:SetParent(self)
+	redlight:SetParent( self )
 	redlight:SetKeyValue( "_light", "255 0 0 255" )  
-	redlight:SetKeyValue("distance", "150" )
+	redlight:SetKeyValue( "distance", "150" )
 	redlight:Spawn()
 end
 
 function ENT:Use( caller, activator )
-	if timer.Exists("OutlandTimer") then DarkRP.notify( caller, 1, 5, "You must wait until the 30 minute ceasefire has ended." ) return end
-	if GetConVar("blowout_enabled"):GetInt() == 0 then DarkRP.notify( caller, 1, 6, "ERROR: Nothing was detected to cancel." ) return end
+	if timer.Exists( "OutlandTimer" ) then DarkRP.notify( caller, 1, 5, "You must wait until the 30 minute ceasefire has ended." ) return end
+	if !GetConVar( "blowout_enabled" ):GetBool() then DarkRP.notify( caller, 1, 6, "ERROR: Nothing was detected to cancel." ) return end
 	RunConsoleCommand( "blowout_enabled", 0 )
 	timer.Remove( "blowoutcombine" )
 	timer.Remove( "changelevelcombine" )
-	for k, ply in pairs(player.GetAll() ) do
-		DarkRP.notify( ply, 1, 8, "Portal failed to successfully close, emergency cancellation button was activated." )
-	end
+	DarkRP.notifyAll( 1, 8, "Portal failed to successfully close, emergency cancellation button was activated." )
 end
--- addons/Combine Portal Closing Entity/lua/entities/satellite_control/shared.lua
+++ addons/Combine Portal Closing Entity/lua/entities/satellite_control/shared.lua
@@ -1,12 +1,7 @@
 ENT.Type = "anim"
 ENT.Base = "base_gmodentity"
-
 ENT.PrintName = "Satellite Control"
 ENT.Author = "Lambda Gaming"
-ENT.Contact = ""
-ENT.Purpose = "closes portal"
-ENT.Instructions = "" 
 ENT.Category = "Superadmin Only"
-
 ENT.Spawnable = true
 ENT.AdminOnly = true
