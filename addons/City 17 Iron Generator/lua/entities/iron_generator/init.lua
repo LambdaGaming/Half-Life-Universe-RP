e7020c665492faaf2eb55036beb26d62d74d7ddb
LambdaGaming <starwars18m@gmail.com>
2020-02-04 20:40:50 -0500

    Optimization

--- addons/City 17 Iron Generator/lua/entities/iron_generator/init.lua
+++ addons/City 17 Iron Generator/lua/entities/iron_generator/init.lua
@@ -2,10 +2,10 @@ AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
 include('shared.lua')
 
-function ENT:SpawnFunction( ply, tr )
+function ENT:SpawnFunction( ply, tr, name )
 	if !tr.Hit then return end
 	local SpawnPos = tr.HitPos + tr.HitNormal * 1
-	local ent = ents.Create( "iron_generator" )
+	local ent = ents.Create( name )
 	ent:SetPos( SpawnPos )
 	ent:Spawn()
 	ent:Activate()
@@ -26,33 +26,32 @@ function ENT:Initialize()
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_NONE )
 	self:SetSolid( SOLID_VPHYSICS )
-	self:SetUseType(SIMPLE_USE)
+	self:SetUseType( SIMPLE_USE )
  
     local phys = self:GetPhysicsObject()
-	if (phys:IsValid()) then
+	if phys:IsValid() then
 		phys:Sleep()
 	end
-	
-	self:SetHealth(300)
-	self:SetMaxHealth(300)
+
+	self:SetHealth( 300 )
 end
 
 function ENT:Use(caller, activator)
-	if timer.Exists("iron_gen_timer") then DarkRP.notify( caller, 1, 6, "The generator is still in the process of producing the iron!" ) return end
+	if timer.Exists( "iron_gen_timer" ) then DarkRP.notify( caller, 1, 6, "The generator is still in the process of producing the iron!" ) return end
 	for i=1, 5 do
-		local iron = ents.Create("ironbar")
+		local iron = ents.Create( "ironbar" )
 		iron:SetPos( self:GetPos() + Vector( 0, 0, 100 + ( i * 4 ) ) )
 		iron:SetCollisionGroup( COLLISION_GROUP_PLAYER )
 		iron:Spawn()
 	end
-	self:EmitSound("gen_sound")
-	timer.Simple( 10, function() self:StopSound("gen_sound") self:EmitSound("vehicles/Airboat/fan_motor_shut_off1.wav") end )
+	self:EmitSound( "gen_sound" )
+	timer.Simple( 10, function() self:StopSound("gen_sound") self:EmitSound( "vehicles/Airboat/fan_motor_shut_off1.wav" ) end )
 	timer.Create( "iron_gen_timer", 300, 1, function() end )
 end
 
-function ENT:OnTakeDamage()
-	local dmg = DamageInfo():GetDamage()
-	self:SetHealth( self:Health() - dmg )
+function ENT:OnTakeDamage( dmg )
+	local amt = dmg:GetDamage()
+	self:SetHealth( self:Health() - amt )
 	if self:Health() <= 0 then
 		local explode = ents.Create("env_explosion")
 		explode:SetPos( self:GetPos() )
