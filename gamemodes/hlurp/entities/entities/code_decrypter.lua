f4115fa69d59d2592a823d87050388a7583da54a
OPGman <28662514+LambdaGaming@users.noreply.github.com>
2025-02-02 11:56:09 -0500

    Add material to code decrypter model

--- gamemodes/hlurp/entities/entities/code_decrypter.lua
+++ gamemodes/hlurp/entities/entities/code_decrypter.lua
@@ -8,29 +8,16 @@ ENT.Spawnable = true
 ENT.AdminOnly = true
 ENT.Category = "HLU RP"
 
-function ENT:SpawnFunction( ply, tr, name )
-	if !tr.Hit then return end
-	local SpawnPos = tr.HitPos + tr.HitNormal * 1
-	local ent = ents.Create( name )
-	ent:SetPos( SpawnPos )
-	ent:Spawn()
-	ent:Activate()
-	return ent
-end
-
 function ENT:Initialize()
-    self:SetModel( "models/props_c17/TrapPropeller_Lever.mdl" )
+	self:SetModel( "models/props_c17/TrapPropeller_Lever.mdl" )
+	self:SetMaterial( "models/weapons/v_slam/new light1" )
 	self:SetMoveType(MOVETYPE_VPHYSICS)
 	self:SetSolid(SOLID_VPHYSICS)
 	if SERVER then
 		self:PhysicsInit(SOLID_VPHYSICS)
 		self:SetUseType(SIMPLE_USE)
 	end
- 
-    local phys = self:GetPhysicsObject()
-	if phys:IsValid() then
-		phys:Wake()
-	end
+	self:PhysWake()
 end
 
 function ENT:Use( ply )
